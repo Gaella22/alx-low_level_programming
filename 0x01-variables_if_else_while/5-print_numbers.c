8c23c56d8551cdef223442bcd53166c6168cf943
INEZA Lyse Gaella <nelyga01@gmail.com>
2023-08-11 16:25:23 +0200

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *Description: Numbers of base 10
+  *Return: Always 0
+  */
+int main(void)
+{
+	int nbr;
+
+	for (nbr = 0; nbr <= 9; nbr++)
+	{
+		printf("%d", nbr);
+	}
+	printf("\n");
+	return (0);
+}
