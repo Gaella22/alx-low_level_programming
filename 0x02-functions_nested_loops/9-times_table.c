50046f2c03632867921802e70f7c0e6283c31709
INEZA Lyse Gaella <nelyga01@gmail.com>
2023-08-15 14:50:43 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * times_tables - print table 9 times
+ *
+ *
+ */
+void times_tables(void)
+{
+	int n1, n2, tbl;
+
+	for (n1 = 1; n1 <= 9; n1++)
+	{
+		_putchar(48);
+		for (n2 = 1; n2 <= 9; n2++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			tbl = n1 * n2;
+			if (tbl <= 9)
+				_putchar(' ');
+			else
+				_putchar((tbl / 10) + 48);
+			_putchar((tbl % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
