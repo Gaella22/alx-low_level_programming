64e35753e9882c39c2918ea2e433bb24ae533fe2
INEZA Lyse Gaella <nelyga01@gmail.com>
2023-08-15 08:52:11 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *Description: Alphabet
+ *Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	int y;
+
+	for (x = 1; i <= 10; i++)
+	{
+		for (y = 97; y <= 122; y++)
+		{
+			_puchar(y);
+		}
+		_putchar('\n');
+	}
+}
