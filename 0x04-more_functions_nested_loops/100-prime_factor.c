404d2e0636297ef17b04551729ee3eb512ffbd34
INEZA Lyse Gaella <nelyga01@gmail.com>
2023-08-17 15:36:51 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,59 +1,22 @@
 #include <stdio.h>
+#include <math.h>
 /**
- * _sqrt - square root
- *
- * @x: number;
- *
- * Return: square root of x
- *
-*/
-double _sqrt(double x)
-{
-	float sqrt, tmp;
-
-	sqrt = x / 2;
-	tmp = 0;
-	while (sqrt != tmp)
-	{
-		tmp = sqrt;
-		sqrt = (x / tmp + tmp) / 2;
-	}
-	return (sqrt);
-}
-
-/**
- * largest_prime_factor - finds and prints the largest number
- *
- *
- * @n: number to find
- *
-*/
-void largest_prime_factor(long int n)
-{
-	int p, l;
-
-	while (n % 2 == 0)
-		n = n / 2;
-	for (p = 3; p <= _sqrt(n); p += 2)
-	{
-		while (n % p == 0)
-		{
-			n = n / p;
-			l = p;
-		}
-	}
-	if (n > 2)
-		l = n;
-	printf("%ld\n", l);
-}
-
-/**
- * main - Entry point
- *
- * Return: always 0
+* main - largest number
+* Return: always 0
 */
 int main(void)
 {
-	largest_prime_factor(353535363637789);
+	long l, ml;
+	long nbr = 93948576201844;
+	double square = sqrt(nbr);
+
+	for (l = 1; l <= square; l++)
+	{
+		if (nbr % l == 0)
+		{
+			ml = nbr / l;
+		}
+	}
+	printf("%ld", ml);
 	return (0);
 }
