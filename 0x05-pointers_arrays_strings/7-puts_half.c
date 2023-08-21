5e8134376aee5a08436c9339a2a298097c20ea2c
INEZA Lyse Gaella <nelyga01@gmail.com>
2023-08-21 13:43:38 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,17 +1,32 @@
 #include "main.h"
 /**
- * _strlen - length of a string
- * @s: string
+ * puts_half - prints half of a string
+ * @str: string
  * Return: length
 */
 void puts_half(char *str)
 {
-	int l = 0;
+	int l, m, a;
 
-	while (*s != '\0')
+	l = 0;
+
+	while (str[l] != '\0')
 	{
 		l++;
-		s++;
 	}
-	return (l);
+	if (l % 2 == 0)
+	{
+		for (a = l / 2; str[a] != '\0'; a++)
+		{
+			_putchar(str[a]);
+		}
+	}
+	else if (l % 2)
+	{
+		for (m = (l - 1) / 2; m < l - 1; m++)
+		{
+			_putchar(str[m + 1]);
+		}
+	}
+	_putchar('\n');
 }
