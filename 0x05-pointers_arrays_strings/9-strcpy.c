b6d4f951645dbdb62304c9b7765c5728d7f00d07
INEZA Lyse Gaella <nelyga01@gmail.com>
2023-08-21 13:25:36 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,14 +7,16 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-	int l = 0;
-	int x = 0;
+	int l;
+	int x;
 
-	while (*(src + 1) != '\0')
+	l = 0;
+
+	while (src[l] != '\0')
 	{
 		l++;
 	}
-	for ( ; x < l; x++)
+	for (x = 0 ; x < l; x++)
 	{
 		dest[x] = src[x];
 	}
