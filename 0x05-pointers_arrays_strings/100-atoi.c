932e95a2786ec725c763a8ec1fabae3fac3af385
INEZA Lyse Gaella <nelyga01@gmail.com>
2023-08-21 14:01:54 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,3 +1,40 @@
 #include "main.h"
 /**
- *
+ * _atoi - converts a string to an integer
+ * @s: string
+ * Return: int converted from the string
+*/
+int _atoi(char *s)
+{
+	int a, b, m, l, e, d;
+
+	a = 0;
+	b = 0;
+	m = 0;
+	l = 0;
+	e = 0;
+	d = 0;
+
+	while (s[l] != '\0')
+		l++;
+	while (a < l && e == 0)
+	{
+		if (s[a] == '-')
+			b++;
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			d = s[a] - '0';
+			if (b % 2)
+				d = -d;
+			m = m * 10 + digit;
+			e = 1;
+			if (s[a + 1] < '0' || s[a + 1] > '9')
+				break;
+			e = 0;
+		}
+		a++;
+	}
+	if (e == 0)
+		return (0);
+	return (m);
+}
